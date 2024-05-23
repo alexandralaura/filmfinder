3f3a3d72ec29020681eac5e439ffb288876d7442
Alexandra Irwin <airwin357@gmail.com>
2024-05-24 08:47:55 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,26 @@ import './App.css';
 
 function App() {
   return (
+    <>
+    <body>
+    <header>
+      <h1 id="appTitle">🍿Film Finder🍿</h1>
+    </header>
+    <form id="genreForm">
+      <label>Choose a genre:</label>
+      <select name="genres" id="genres">
+      </select>
+    </form>
+    <button id="playBtn">Let's Play!</button>
+    <div id="movieInfo">
+      <div id="moviePoster"></div>
+      <div id="movieText"></div>
+    </div>
+    <div id="likeOrDislikeBtns" hidden>
+      <button id="likeBtn"><i class="fa-solid fa-thumbs-up"></i></button>
+      <button id="dislikeBtn"><i class="fa-solid fa-thumbs-down"></i></button>
+    </div>
+  </body>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +39,7 @@ function App() {
         </a>
       </header>
     </div>
+    </>  
   );
 }
 
